1081805855bcd2a5718caa214d859c23c1b52b0f
Klaudiusz Fabryczny <klaudiusz.fabryczny@sap.com>
2026-01-19 13:19:20 +0100

    Increase MaxConcurrentReconciles to 4

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/config"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
@@ -96,7 +97,7 @@ func main() {
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "absent-metrics-operator.cloud.sap",
-	})
+		Controller:             config.Controller{MaxConcurrentReconciles: 4}})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
